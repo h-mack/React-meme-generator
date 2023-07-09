d394221e319b770f1b9b273a966dccdb2c0c9ae7
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2023-07-10 09:14:03 +1000

    Swap file names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,70 +1,14 @@
 import React from "react"
 
-export default function Meme() {
-  const [meme, setMeme] = React.useState({
-    topText: "",
-    bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg"
-  })
-  const [allMemes, setAllMemes] = React.useState([])
-
-  React.useEffect(() => {
-    async function getMemes() {
-      const res = await fetch("https://api.imgflip.com/get_memes")
-      const data = await res.json()
-      setAllMemes(data.data.memes)
-    }
-    getMemes()
-  }, [])
-
-  function getMemeImage() {
-    const randomNumber = Math.floor(Math.random() * allMemes.length)
-    const url = allMemes[randomNumber].url
-    setMeme(prevMeme => ({
-      ...prevMeme,
-      randomImage: url
-    }))
-  }
-
-  function handleChange(event) {
-    const { name, value } = event.target
-    setMeme(prevMeme => ({
-      ...prevMeme,
-      [name]: value
-    }))
-  }
-
+export default function Header() {
   return (
-    <main>
-      <div className="form">
-        <input
-          type="text"
-          placeholder="Top text"
-          className="form--input"
-          name="topText"
-          value={meme.topText}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Bottom text"
-          className="form--input"
-          name="bottomText"
-          value={meme.bottomText}
-          onChange={handleChange}
-        />
-        <button
-          className="form--button"
-          onClick={getMemeImage}
-        >
-          Get a new meme image
-        </button>
-      </div>
-      <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>
-      </div>
-    </main>
+    <header className="header">
+      <img
+        src="/troll-face.png"
+        className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
   )
 }
